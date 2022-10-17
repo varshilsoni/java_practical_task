347f59d53dff90d6791cc17115c905069897dd5b
varshilsoni <sonivarshil11@gmail.com>
2022-10-17 16:54:27 +0530

    program to find maximum number

---
+++ MaxNumber.java
@@ -0,0 +1,29 @@
+import static java.lang.System.*;
+import java.util.Scanner;  
+
+class Main
+{
+
+public static void main(String[] args){ 
+        
+		int no, max;
+
+        Scanner sc = new Scanner(System.in);
+        out.println("how many number you want to enter : ");
+        no = sc.nextInt();
+        int array[] = new int[no];
+        
+        out.println("enter array element : ");
+        for(int i=0; i<no; i++){
+            array[i] = sc.nextInt();
+        }
+        max=array[0];
+        for (int i =0; i<no; i++){
+            if(max < array[i]){
+                
+                max =array[i];
+            }
+        }
+        out.println("max number is : " + max);
+    }
+}
